d730c020251b935ee90102f164aeae92f8e57481
Soumitra <projwebinfo@gmail.com>
2025-10-23 08:52:42 +0530

    Create OrderRequest.java

---
+++ microservices-orchestration/order-service/src/main/java/com/roytuts/orderservice/dto/OrderRequest.java
@@ -0,0 +1,33 @@
+package com.roytuts.orderservice.dto;
+
+public class OrderRequest {
+
+	private String productId;
+	private int quantity;
+	private String userId;
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}

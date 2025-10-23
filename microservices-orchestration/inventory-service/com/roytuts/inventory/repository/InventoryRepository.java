d1a4fa8d0358f8564cea378e40eb1ecba5004513
Soumitra <projwebinfo@gmail.com>
2025-10-23 08:40:40 +0530

    Delete microservices-orchestration/inventory-service/com/roytuts/inventory directory

--- microservices-orchestration/inventory-service/com/roytuts/inventory/repository/InventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.roytuts.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.roytuts.inventory.model.InventoryItem;
-
-public interface InventoryRepository extends JpaRepository<InventoryItem, String> {
-}

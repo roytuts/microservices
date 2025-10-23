1cb35f6c2402349ab2ce89ae610e13dd35b282ab
Soumitra <projwebinfo@gmail.com>
2025-10-23 08:46:26 +0530

    Create OrchestratorServiceApplication.java

---
+++ microservices-orchestration/orchestrator-service/src/main/java/com/roytuts/orchestrator/OrchestratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roytuts.orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OrchestratorServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestratorServiceApplication.class, args);
+	}
+}
